d8910af5fb406e38baad96ddb7b5b83b6c3838cf
noIce213 <159175068+noIce213@users.noreply.github.com>
2024-02-08 14:10:55 +0000

    Added some comments to my code

--- Final Project.js
+++ Final Project.js
@@ -1,13 +1,13 @@
-const startMenu = document.getElementById("start-menu");
+const startMenu = document.getElementById("start-menu"); // Gets both menus
 const nameInput = document.getElementById("name-input");
-
+// Change whatever you want here to make it look nicer/work more around your "range"
 function playerNameSelect() {
-    gameMode = document.getElementById("game-mode").value;
+    gameMode = document.getElementById("game-mode").value; //Gets the mode you picked
 
-    startMenu.style.display = "none";
-    nameInput.style.display = "block";
+    startMenu.style.display = "none"; //Disables the start menu
+    nameInput.style.display = "block"; //Enables the name select
 
-    if (gameMode === "robot") {
+    if (gameMode === "robot") { // if you picked robot, disables the player 2 name input
         document.getElementById("player2").value = "Robot";
         document.getElementById("player2").disabled = true;
     }
