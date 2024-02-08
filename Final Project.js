c1ef79e19dedb3d78f2b88fb703282e338fbe030
noIce213 <159175068+noIce213@users.noreply.github.com>
2024-02-08 14:04:16 +0000

    Added a second menu for name select and a function to change between them

--- Final Project.js
+++ Final Project.js
@@ -1,1 +1,14 @@
+const startMenu = document.getElementById("start-menu");
+const nameInput = document.getElementById("name-input");
 
+function playerNameSelect() {
+    gameMode = document.getElementById("game-mode").value;
+
+    startMenu.style.display = "none";
+    nameInput.style.display = "block";
+
+    if (gameMode === "robot") {
+        document.getElementById("player2").value = "Robot";
+        document.getElementById("player2").disabled = true;
+    }
+}
