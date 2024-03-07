577aceabc509d013f24f86e6dfb97bb435b78d74
Bamood <159316596+Bamood@users.noreply.github.com>
2024-03-07 19:03:36 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,17 +1,18 @@
 var startmenu = document.getElementById("start-menu"); // Gets menus
 var nameinput = document.getElementById("name-input");
-var diffmenu = document.getElementById("diff-select")
+var diffmenu = document.getElementById("diff-select");
 // Change whatever you want here to make it look nicer/work more around your "range"
 
 function diff(){//difficulty menu
     gamemode = document.getElementById("game-mode").value; //Gets the mode you picked
 
     startmenu.style.display = "none"; //Disables the start menu
+    diffmenu.style.display = "block"; //Enables difficulty menu
 
 }
 
 function playerNameSelect(){
-    difficulty = document.getElementById("diff").value
+    difficulty = document.getElementById("diff").value;
     diffmenu.style.display = "none"; //Disables diff menu
     nameinput.style.display = "block"; //Enables the name select
 
