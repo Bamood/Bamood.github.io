703075b872f71ef4054511da95f40af09a17131f
Bamood <159316596+Bamood@users.noreply.github.com>
2024-03-07 08:28:45 +0000

    Progress 1

--- Final Project.js
+++ Final Project.js
@@ -1,14 +1,26 @@
-const startMenu = document.getElementById("start-menu"); // Gets both menus
-const nameInput = document.getElementById("name-input");
+var startmenu = document.getElementById("start-menu"); // Gets menus
+var nameinput = document.getElementById("name-input");
+var diffmenu = document.getElementById("diff-select")
 // Change whatever you want here to make it look nicer/work more around your "range"
-function playerNameSelect() {
-    gameMode = document.getElementById("game-mode").value; //Gets the mode you picked
 
-    startMenu.style.display = "none"; //Disables the start menu
-    nameInput.style.display = "block"; //Enables the name select
+function diff(){//difficulty menu
+    gamemode = document.getElementById("game-mode").value; //Gets the mode you picked
+
+    startmenu.style.display = "none"; //Disables the start menu
+
+}
+
+function playerNameSelect(){
+    difficulty = document.getElementById("diff").value
+    diffmenu.style.display = "none"; //Disables diff menu
+    nameinput.style.display = "block"; //Enables the name select
 
     if (gameMode === "robot") { // if you picked robot, disables the player 2 name input
         document.getElementById("player2").value = "Robot";
         document.getElementById("player2").disabled = true;
     }
+}
+
+function startgame(){
+
 }
