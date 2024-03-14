c4882d88eed129aec6d4556530481c01cde05b8b
Bamood <159316596+Bamood@users.noreply.github.com>
2024-03-14 08:35:55 +0000

    Fixed page loading

--- index.js
+++ index.js
@@ -1,25 +1,24 @@
-var startmenu = document.getElementById("start-menu"); // Gets menus
-var nameinput = document.getElementById("name-input");
-var diffmenu = document.getElementById("diff-select");
-var gamemode = document.getElementById("game-mode").value; //Gets the mode you picked
 // Change whatever you want here to make it look nicer/work more around your "range"
 
 function diff(){//difficulty menu
-    
-    if (gamemode == "robot") {
-        startmenu.style.display = "none"; //Enables the name select
-        diffmenu.style.display = "block"; //Disables diff menu
+
+    var gamemode = document.getElementById("game-mode").value; //Gets the mode you picked
+    if (gamemode == "human") {
+        document.getElementById("start-menu").style.display="none"; //Disables diff menu
+        document.getElementById("name-input").style.display="block"; //Enables the name select
+        
     } else
     {
-        startmenu.style.display = "none"; //Disables diff menu
-        nameinput.style.display = "block"; //Enables the name select
+        document.getElementById("start-menu").style.display="none";//Disables start menu
+        document.getElementById("diff-select").style.display="block"; //Enables the name select
     }
 
 }
 
 function playerNameSelect(){
-    diffmenu.style.display = "none"; //Disables diff menu
-    nameinput.style.display = "block"; //Enables the name select
+    var gamemode = document.getElementById("game-mode").value; //Gets the mode you picked
+    document.getElementById("diff-select").style.display="none"; //Disables diff menu
+    document.getElementById("name-input").style.display="block"; //Enables the name select
 
     if (gamemode == "robot") { // if you picked robot, disables the player 2 name input
         document.getElementById("player2").value = "Robot";
@@ -29,4 +28,7 @@ function playerNameSelect(){
 
 function startgame(){
 
+    
+
 }
+
